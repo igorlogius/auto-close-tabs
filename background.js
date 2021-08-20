fecfbb7d7f21e57f6b757c8950701724385dd1b5
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-21 00:19:57 +0200

    feat: add options page to set config values

--- background.js
+++ background.js
@@ -2,73 +2,107 @@ const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
-async function tabCleanUp(){
-	console.log('tabCleanUp()');
-	// get non-hidden, non-activ tabs 
-	const tabs = await browser.tabs.query({
-		active: false,  // ignore active
-		hidden: false,  // ignore hidden
-		audible: false, // ignore playing sounds
-		highlighted: false, // ignnore highlighted
-		pinned: false, // ignore pinned
-	});
-	if(tabs.length < 8) {return;}
-
-	// check idle time
-	const epoch_now = new Date().getTime();
-	for(const tab of tabs) {
-
-		// check last activation time
-		const delta = epoch_now - tab.lastAccessed
-		if( delta > (temporary?5000:1000*60*15) ){ // every 3 minutes in debug, else every 15
-
-
-			if( tab.url.startsWith('http')) {
-
-
-				console.log('found tab with idletime > 3/15 minutes', tab.url);
-
-				// check if tab contains input fields
-				try {
-					let mightHaveUserInput = await browser.tabs.executeScript(tab.id, {
-						code: `(function(){
-					let els = document.querySelectorAll('input[type="text"],input[type="password"]');
-					for(const el of els) {
-						console.log(JSON.stringify(el.classList), el.value);
-						if(el.type !== 'hidden' && el.style.display !== 'none' && typeof el.value === 'string' && el.value !== ''){
-							return true;
-						}
-					}
-
-					els = document.querySelectorAll('textarea');
-					for(const el of els) {
-						console.log(JSON.stringify(el.classList), el.value);
-						if(el.style.display !== 'none' && typeof el.value === 'string' && el.value !== ''){
-							return true;
-						}
-					}
-					return false;
-				  }());`
-					});
-					mightHaveUserInput = mightHaveUserInput[0];
-
-					if(!mightHaveUserInput){
-						console.log('removed tab ', tab.id, tab.url);
-						await browser.tabs.remove(tab.id);
-					}
-				}catch(e){
-					console.log('error',e);
-				}
-			}else{
-				console.log('removed tab ', tab.id, tab.url);
-				await browser.tabs.remove(tab.id);
+async function getFromStorage(storeid,fallback) {
+	return (await (async () => {
+		try {
+			//console.log('storeid', storeid)
+			let tmp = await browser.storage.local.get(storeid);
+			//console.log(tmp);
+			if (typeof tmp[storeid] !== 'undefined'){
+				return tmp[storeid];
+				
 			}
-		}else{
-			console.log('tab', tab.id, tab.url, ' no http proto');
+		}catch(e){
+			console.error(e);
+		}
+		return (fallback)?fallback:undefined;
+	})());
+}
+
+async function tabCleanUp(){
+
+	// get non active, hidden, audible, highlighted or pinned tabs
+	const tabs = await browser.tabs.query({
+		active: false,  
+		hidden: false,  
+		audible: false, 
+		highlighted: false, 
+		pinned: false, 
+	});
+
+
+	const closeThreshold = await getFromStorage('closeThreshold', 7)
+	//console.log('closeThreshold', closeThreshold);
+
+	if(tabs.length > closeThreshold) {
+
+		let nb_of_tabs_to_close = tabs.length - closeThreshold;
+			
+		if(nb_of_tabs_to_close < 1) { return; }
+
+		// check idle time
+		const epoch_now = new Date().getTime();
+		const minIdleTime = await getFromStorage('minIdleTime', 1000*60*15)
+
+		//console.log('minIdleTime', minIdleTime);
+
+		tabs.sort((a,b) => {a.lastAccessed - b.lastAccessed});
+
+		for(const tab of tabs) {
+
+			if(nb_of_tabs_to_close < 1) { break; }
+			nb_of_tabs_to_close--;
+
+			// check last activation time
+			const delta = epoch_now - tab.lastAccessed
+			if( delta > (temporary?5000:minIdleTime) ){ // every 5 seconds in debug, else every storage value or 15 minutes if not yet set
+
+				if( tab.url.startsWith('http')) {
+
+					try {
+						// check if tab contains potential text fields with user input
+						// exclude hidden and non visible stuff
+						let mightHaveUserInput = await browser.tabs.executeScript(tab.id, {
+							code: 
+							`(function(){
+								let els = document.querySelectorAll('input[type="text"],input[type="password"]');
+								for(const el of els) {
+									if(         el.type !== 'hidden' && 
+									   el.style.display !== 'none'   && 
+									    typeof el.value === 'string' && 
+										   el.value !== ''
+									){
+										return true;
+									}
+								}
+								els = document.querySelectorAll('textarea');
+								for(const el of els) {
+									if( el.style.display !== 'none'   && 
+									     typeof el.value === 'string' && 
+										    el.value !== ''
+									){
+										return true;
+									}
+								}
+								return false;
+							  }());`
+						});
+						mightHaveUserInput = mightHaveUserInput[0];
+
+						if(!mightHaveUserInput){
+							await browser.tabs.remove(tab.id);
+						}
+					}catch(e){
+						console.error(e);
+					}
+				}else{
+					await browser.tabs.remove(tab.id);
+				}
+			}
 		}
 	}
 }
 
-setInterval(tabCleanUp, (temporary?5000:5*60*500)); // check every minute in debug, else every 5 minutes
+setInterval(tabCleanUp, (temporary?5000:3*60*1000)); // check every 5 seconds in debug, else every 3 minutes
 
 
