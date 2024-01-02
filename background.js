7b03f1b80bac769fd1fc0d01daf985e088fb3e06
igorlogius <67047467+igorlogius@users.noreply.github.com>
2024-01-03 00:36:38 +0100

    fix: set default toolbar area chore: refactor code

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /* global browser */
 
-const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const temporary = browser.runtime.id.endsWith("@temporary-addon"); // debugging?
 const excluded_tabs = new Set();
 const included_windows = new Set();
 
@@ -14,96 +14,102 @@ let setIntervalId = null;
 let multipleHighlighted = false;
 
 // default state toolbar button/icon
-browser.browserAction.setBadgeText({text:'off'});
-browser.browserAction.setBadgeBackgroundColor({color:'red'});
+browser.browserAction.setBadgeText({ text: "off" });
+browser.browserAction.setBadgeBackgroundColor({ color: "red" });
 
 async function getFromStorage(type, id, fallback) {
-    let tmp = await browser.storage.local.get(id);
-    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
 }
 
-async function isWhitelisted(url){
-
-    const selectors = await ((async () => {
-        try {
-            const tmp = await browser.storage.local.get('selectors');
-            if(typeof tmp['selectors'] !== 'undefined') {
-                return tmp['selectors'];
-            }
-        }catch(e){
-            console.error(e);
-        }
-        return [];
-    })());
-
-    for(const selector of selectors) {
-        try {
-            if(    typeof selector.activ === 'boolean'
-                && selector.activ === true
-                && typeof selector.url_regex === 'string'
-                && selector.url_regex !== ''
-                && (new RegExp(selector.url_regex)).test(url)
-            ){
-                return true;
-            }
-        }catch(e){
-            console.error(e);
-        }
+async function isWhitelisted(url) {
+  const selectors = await (async () => {
+    try {
+      const tmp = await browser.storage.local.get("selectors");
+      if (typeof tmp["selectors"] !== "undefined") {
+        return tmp["selectors"];
+      }
+    } catch (e) {
+      console.error(e);
     }
-    return false;
+    return [];
+  })();
+
+  for (const selector of selectors) {
+    try {
+      if (
+        typeof selector.activ === "boolean" &&
+        selector.activ === true &&
+        typeof selector.url_regex === "string" &&
+        selector.url_regex !== "" &&
+        new RegExp(selector.url_regex).test(url)
+      ) {
+        return true;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return false;
 }
 
-async function tabCleanUp(){
+async function tabCleanUp() {
+  console.debug("tabCleanUp");
 
-	const qryobj =  {
-		active: false,
-		hidden: false,
-		audible: false,
-		highlighted: false,
-		pinned: false
-	};
+  const qryobj = {
+    active: false,
+    hidden: false,
+    audible: false,
+    highlighted: false,
+    pinned: false,
+  };
 
-	// get non active, hidden, audible, highlighted or pinned tabs 
-	// which are not excluded or in an excluded Window
-	// or which match a whitelist expression
-	let tabs = await browser.tabs.query(qryobj);
-	tabs = tabs.filter( (t) => (included_windows.has(t.windowId)));
-	tabs = tabs.filter( (t) => (!excluded_tabs.has(t.id)));
-	tabs = tabs.filter( async (t) => (!(await isWhitelisted(t.url))));
+  // get non active, hidden, audible, highlighted or pinned tabs
+  // which are not excluded or in an excluded Window
+  // or which match a whitelist expression
+  let tabs = await browser.tabs.query(qryobj);
+  tabs = tabs.filter((t) => included_windows.has(t.windowId));
+  tabs = tabs.filter((t) => !excluded_tabs.has(t.id));
+  tabs = tabs.filter(async (t) => !(await isWhitelisted(t.url)));
 
-	if(onlyClosePrivateTabs){
-		tabs = tabs.filter( t => t.incognito );
-	}
+  console.debug("considering tabs", tabs);
 
-	if(tabs.length > closeThreshold) {
+  if (onlyClosePrivateTabs) {
+    tabs = tabs.filter((t) => t.incognito);
+  }
 
-		let nb_of_tabs_to_close = tabs.length - closeThreshold;
+  if (tabs.length > closeThreshold) {
+    let nb_of_tabs_to_close = tabs.length - closeThreshold;
 
-		if(nb_of_tabs_to_close < 1) { return; }
+    if (nb_of_tabs_to_close < 1) {
+      return;
+    }
 
-		// check idle time
-		const epoch_now = new Date().getTime();
-		const minIdleTimeMilliSecs = minIdleTime*minIdleTimeUnit;
+    // check idle time
+    const epoch_now = new Date().getTime();
+    const minIdleTimeMilliSecs = minIdleTime * minIdleTimeUnit;
 
-		tabs.sort((a,b) => {a.lastAccessed - b.lastAccessed});
+    tabs.sort((a, b) => {
+      a.lastAccessed - b.lastAccessed;
+    });
 
-		for(const tab of tabs) {
+    for (const tab of tabs) {
+      if (nb_of_tabs_to_close < 1) {
+        break;
+      }
+      nb_of_tabs_to_close--;
 
-			if(nb_of_tabs_to_close < 1) { break; }
-			nb_of_tabs_to_close--;
+      // check last activation time
+      const delta = epoch_now - tab.lastAccessed;
+      if (delta > (temporary ? 5000 : minIdleTimeMilliSecs)) {
+        // every 5 seconds in debug, else every storage value or 15 minutes if not yet set
 
-			// check last activation time
-			const delta = epoch_now - tab.lastAccessed
-			if( delta > (temporary?5000:minIdleTimeMilliSecs) ){ // every 5 seconds in debug, else every storage value or 15 minutes if not yet set
-
-				if( tab.url.startsWith('http')) {
-
-					try {
-						// check if tab contains potential text fields with user input
-						// exclude hidden and non visible stuff
-						let mightHaveUserInput = await browser.tabs.executeScript(tab.id, {
-							code:
-							`(function(){
+        if (tab.url.startsWith("http")) {
+          try {
+            // check if tab contains potential text fields with user input
+            // exclude hidden and non visible stuff
+            let mightHaveUserInput = await browser.tabs.executeScript(tab.id, {
+              code: `(function(){
 								let els = document.querySelectorAll('input[type="text"],input[type="password"]');
 								for(const el of els) {
 									if(         el.type !== 'hidden' &&
@@ -124,119 +130,143 @@ async function tabCleanUp(){
 									}
 								}
 								return false;
-							  }());`
-						});
-						mightHaveUserInput = mightHaveUserInput[0];
+							  }());`,
+            });
+            mightHaveUserInput = mightHaveUserInput[0];
 
-						if(!mightHaveUserInput){
-                            try {
-								if(typeof saveFolder === 'string' && saveFolder !== ''){
-									let createdetails = {
-										title: tab.title,
-										url: tab.url,
-										parentId: saveFolder
-									}
-									browser.bookmarks.create(createdetails);
-								}
-                            }catch(e) {
-                                console.error(e);
-                            }
-							await browser.tabs.remove(tab.id);
-						}
-					}catch(e){
-						console.error(e, tab.url);
-					}
-				}else{
-					await browser.tabs.remove(tab.id);
-				}
-			}
-		} // for tabs
-	}
+            console.debug(
+              "tab",
+              tab.id,
+              "mightHaveUserInput",
+              mightHaveUserInput
+            );
+            if (!mightHaveUserInput) {
+              try {
+                if (typeof saveFolder === "string" && saveFolder !== "") {
+                  let createdetails = {
+                    title: tab.title,
+                    url: tab.url,
+                    parentId: saveFolder,
+                  };
+                  browser.bookmarks.create(createdetails);
+                }
+              } catch (e) {
+                console.error(e);
+              }
+              await browser.tabs.remove(tab.id);
+            }
+          } catch (e) {
+            console.error(e, tab.url);
+          }
+        } else {
+          await browser.tabs.remove(tab.id);
+        }
+      }
+    } // for tabs
+  }
 }
 
 browser.menus.create({
-	title: "Exclude",
-	contexts: ["tab"],
-	onclick: async (info, tab) => {
-		if(multipleHighlighted){
-			const tabs = (await browser.tabs.query({highlighted:true, currentWindow: true, hidden: false}));
-			for(const t of tabs){
-			    if(!excluded_tabs.has(t.id)){
-				excluded_tabs.add(t.id);
-			    }
-			}
-		}else{
-			if(!excluded_tabs.has(tab.id)){
-				excluded_tabs.add(tab.id);
-			}
-		}
-	}
+  title: "Exclude",
+  contexts: ["tab"],
+  onclick: async (info, tab) => {
+    if (multipleHighlighted) {
+      const tabs = await browser.tabs.query({
+        highlighted: true,
+        currentWindow: true,
+        hidden: false,
+      });
+      for (const t of tabs) {
+        if (!excluded_tabs.has(t.id)) {
+          excluded_tabs.add(t.id);
+        }
+      }
+    } else {
+      if (!excluded_tabs.has(tab.id)) {
+        excluded_tabs.add(tab.id);
+      }
+    }
+  },
 });
 
 browser.menus.create({
-	title: "Include",
-	contexts: ["tab"],
-	onclick: async (info, tab) => {
-		if(multipleHighlighted){
-		const tabs = (await browser.tabs.query({highlighted:true, currentWindow: true, hidden: false}));
-		for(const t of tabs){
-		    if(excluded_tabs.has(t.id)){
-			excluded_tabs.delete(t.id);
-		    }
-		}
-		}else{
-		    if(excluded_tabs.has(tab.id)){
-			excluded_tabs.delete(tab.id);
-		    }
-		}
-	}
+  title: "Include",
+  contexts: ["tab"],
+  onclick: async (info, tab) => {
+    if (multipleHighlighted) {
+      const tabs = await browser.tabs.query({
+        highlighted: true,
+        currentWindow: true,
+        hidden: false,
+      });
+      for (const t of tabs) {
+        if (excluded_tabs.has(t.id)) {
+          excluded_tabs.delete(t.id);
+        }
+      }
+    } else {
+      if (excluded_tabs.has(tab.id)) {
+        excluded_tabs.delete(tab.id);
+      }
+    }
+  },
 });
 
-// include Windows ( better to be safe then sorry ) 
+// include Windows ( better to be safe then sorry )
 async function onBAClicked(tab) {
-	if(included_windows.has(tab.windowId)){
-		included_windows.delete(tab.windowId);
-		browser.browserAction.setBadgeText({text:'off', windowId: tab.windowId});
-		browser.browserAction.setBadgeBackgroundColor({color:'red', windowId: tab.windowId});
-	}else{
-		included_windows.add(tab.windowId);
-		browser.browserAction.setBadgeText({text:'on',windowId: tab.windowId});
-		browser.browserAction.setBadgeBackgroundColor({color:'green', windowId: tab.windowId});
-	}
+  if (included_windows.has(tab.windowId)) {
+    included_windows.delete(tab.windowId);
+    browser.browserAction.setBadgeText({ text: "off", windowId: tab.windowId });
+    browser.browserAction.setBadgeBackgroundColor({
+      color: "red",
+      windowId: tab.windowId,
+    });
+  } else {
+    included_windows.add(tab.windowId);
+    browser.browserAction.setBadgeText({ text: "on", windowId: tab.windowId });
+    browser.browserAction.setBadgeBackgroundColor({
+      color: "green",
+      windowId: tab.windowId,
+    });
+  }
 }
 
 function onTabRemoved(tabId /*, removeInfo*/) {
-	if(excluded_tabs.has(tabId)){
-		excluded_tabs.delete(tabId);
-        }
+  if (excluded_tabs.has(tabId)) {
+    excluded_tabs.delete(tabId);
+  }
 }
 
 function onWindowRemoved(windowId) {
-	if(included_windows.has(windowId)){
-		included_windows.delete(windowId);
-	}
+  if (included_windows.has(windowId)) {
+    included_windows.delete(windowId);
+  }
 }
 
 async function onStorageChanged() {
-	onlyClosePrivateTabs = await getFromStorage('boolean','onlyClosePrivateTabs',false);
-	closeThreshold = await getFromStorage('number','closeThreshold',7);
-	minIdleTime = await getFromStorage('number','minIdleTime',3);
-	minIdleTimeUnit = await getFromStorage('number','minIdleTimeUnit',86400000);
-	saveFolder = await getFromStorage('string','saveFolder','');
+  onlyClosePrivateTabs = await getFromStorage(
+    "boolean",
+    "onlyClosePrivateTabs",
+    false
+  );
+  closeThreshold = await getFromStorage("number", "closeThreshold", 7);
+  minIdleTime = await getFromStorage("number", "minIdleTime", 3);
+  minIdleTimeUnit = await getFromStorage("number", "minIdleTimeUnit", 86400000);
+  saveFolder = await getFromStorage("string", "saveFolder", "");
 
-	if(setIntervalId !== null){
-		clearInterval(setIntervalId);
-		setIntervalId = null;
-	}
-	setIntervalId = setInterval(tabCleanUp, (temporary?10000:10*60*1000));
+  if (setIntervalId !== null) {
+    clearInterval(setIntervalId);
+    setIntervalId = null;
+  }
+  setIntervalId = setInterval(tabCleanUp, temporary ? 10000 : 10 * 60 * 1000);
 }
 
 function onTabsHighlighted(highlightInfo) {
-    multipleHighlighted = (highlightInfo.tabIds.length > 1);
+  multipleHighlighted = highlightInfo.tabIds.length > 1;
 }
 
 (async () => {
-	await onStorageChanged();
+  await onStorageChanged();
 })();
 
 browser.storage.onChanged.addListener(onStorageChanged);
@@ -244,4 +274,3 @@ browser.tabs.onRemoved.addListener(onTabRemoved);
 browser.windows.onRemoved.addListener(onWindowRemoved);
 browser.browserAction.onClicked.addListener(onBAClicked);
 browser.tabs.onHighlighted.addListener(onTabsHighlighted);
-
--- options.js
+++ options.js
@@ -1,93 +1,95 @@
 /* global browser */
 
+const saveFolder = document.getElementById("saveFolder");
 
-const saveFolder = document.getElementById('saveFolder');
-
-function recGetFolders(node, depth = 0){
-    let out = new Map();
-    if(typeof node.url !== 'string'){
-        if(node.id !== 'root________'){
-            out.set(node.id, { 'depth': depth, 'title': node.title });
-        }
-        if(node.children){
-            for(let child of node.children){
-                out = new Map([...out, ...recGetFolders(child, depth+1) ]);
-            }
-        }
+function recGetFolders(node, depth = 0) {
+  let out = new Map();
+  if (typeof node.url !== "string") {
+    if (node.id !== "root________") {
+      out.set(node.id, { depth: depth, title: node.title });
     }
-    return out;
+    if (node.children) {
+      for (let child of node.children) {
+        out = new Map([...out, ...recGetFolders(child, depth + 1)]);
+      }
+    }
+  }
+  return out;
 }
 
 async function initSaveFolderSelect() {
-    const nodes = await browser.bookmarks.getTree();
-    let out = new Map();
-    let depth = 1;
-    for(const node of nodes){
-        out = new Map([...out, ...recGetFolders(node, depth) ]);
-    }
-    for(const [k,v] of out){
-        //console.debug(k, v.title);
-        saveFolder.add(new Option("-".repeat(v.depth) + " " + v.title, k))
-    }
+  const nodes = await browser.bookmarks.getTree();
+  let out = new Map();
+  let depth = 1;
+  for (const node of nodes) {
+    out = new Map([...out, ...recGetFolders(node, depth)]);
+  }
+  for (const [k, v] of out) {
+    //console.debug(k, v.title);
+    saveFolder.add(new Option("-".repeat(v.depth) + " " + v.title, k));
+  }
 }
 
 function onChange(evt) {
+  let id = evt.target.id;
+  let el = document.getElementById(id);
 
-	let id = evt.target.id;
-	let el = document.getElementById(id);
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  let obj = {};
 
-	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
-	let obj = {}
+  if (el.type === "number") {
+    try {
+      value = parseInt(value);
+      if (isNaN(value)) {
+        value = el.min;
+      }
+      if (value < el.min) {
+        value = el.min;
+      }
+    } catch (e) {
+      value = el.min;
+    }
+  }
 
-	if(el.type === 'number'){
-		try {
-			value = parseInt(value);
-			if(isNaN(value)){
-				value = el.min;
-			}
-			if(value < el.min) {
-				value = el.min;
-			}
-		}catch(e){
-			value = el.min
-		}
-	}
-
-	obj[id] = value;
-	console.debug(id,value, el.type);
-	browser.storage.local.set(obj).catch(console.error);
+  obj[id] = value;
+  console.debug(id, value, el.type);
+  browser.storage.local.set(obj).catch(console.error);
 }
 
-
-async function onLoad(){
-    try {
+async function onLoad() {
+  try {
     await initSaveFolderSelect();
-    }catch(e){
-        console.error(e);
-    }
+  } catch (e) {
+    console.error(e);
+  }
 
-[ "onlyClosePrivateTabs", "saveFolder", "closeThreshold","minIdleTime", "minIdleTimeUnit" ].map( (id) => {
+  [
+    "onlyClosePrivateTabs",
+    "saveFolder",
+    "closeThreshold",
+    "minIdleTime",
+    "minIdleTimeUnit",
+  ].map((id) => {
+    browser.storage.local
+      .get(id)
+      .then((obj) => {
+        let el = document.getElementById(id);
+        let val = obj[id];
 
-	browser.storage.local.get(id).then( (obj) => {
+        //console.debug('map', id, val);
 
-		let el = document.getElementById(id);
-		let val = obj[id];
+        if (typeof val !== "undefined") {
+          if (el.type === "checkbox") {
+            el.checked = val;
+          } else {
+            el.value = val;
+          }
+        }
+      })
+      .catch(console.error);
 
-        	//console.debug('map', id, val);
-
-		if(typeof val !== 'undefined') {
-			if(el.type === 'checkbox') {
-				el.checked = val;
-			}
-			else{
-				el.value = val;
-			}
-		}
-
-	}).catch(console.error);
-
-	let el = document.getElementById(id);
-	el.addEventListener('input', onChange);
+    let el = document.getElementById(id);
+    el.addEventListener("input", onChange);
     /*
 	el.addEventListener('keyup', onChange);
 	el.addEventListener('keypress',
@@ -97,149 +99,160 @@ async function onLoad(){
 			}
 		});
     */
-});
+  });
 }
 
 //document.addEventListener('DOMContentLoaded', onLoad);
 
-
 function deleteRow(rowTr) {
-	var mainTableBody = document.getElementById('mainTableBody');
-	mainTableBody.removeChild(rowTr);
+  var mainTableBody = document.getElementById("mainTableBody");
+  mainTableBody.removeChild(rowTr);
 }
 
 function createTableRow(feed) {
-	var mainTableBody = document.getElementById('mainTableBody');
-	var tr = mainTableBody.insertRow();
+  var mainTableBody = document.getElementById("mainTableBody");
+  var tr = mainTableBody.insertRow();
 
-	Object.keys(feed).sort().forEach( (key) => {
+  Object.keys(feed)
+    .sort()
+    .forEach((key) => {
+      if (key === "activ") {
+        var input = document.createElement("input");
+        input.className = key;
+        input.placeholder = key;
+        input.style.width = "100%";
+        input.type = "checkbox";
+        input.checked = typeof feed[key] === "boolean" ? feed[key] : true;
+        tr.insertCell().appendChild(input);
+      } else if (key !== "action") {
+        input = document.createElement("input");
+        input.className = key;
+        input.placeholder = key;
+        input.style.width = "100%";
+        input.value = feed[key];
+        tr.insertCell().appendChild(input);
+      }
+    });
 
-		if( key === 'activ'){
-			var input = document.createElement('input');
-			input.className = key;
-			input.placeholder = key;
-			input.style.width = '100%';
-			input.type='checkbox';
-			input.checked= (typeof feed[key] === 'boolean'? feed[key]: true);
-			tr.insertCell().appendChild(input);
-
-		}else
-			if( key !== 'action'){
-				input = document.createElement('input');
-				input.className = key;
-				input.placeholder = key;
-				input.style.width = '100%';
-				input.value = feed[key];
-				tr.insertCell().appendChild(input);
-			}
-	});
-
-	var button;
-	if(feed.action === 'save'){
-		button = createButton("Add", "saveButton", function() {}, true );
-	}else{
-		button = createButton("Delete", "deleteButton", function() { deleteRow(tr); }, false );
-	}
-	tr.insertCell().appendChild(button);
+  var button;
+  if (feed.action === "save") {
+    button = createButton("Add", "saveButton", function () {}, true);
+  } else {
+    button = createButton(
+      "Delete",
+      "deleteButton",
+      function () {
+        deleteRow(tr);
+      },
+      false
+    );
+  }
+  tr.insertCell().appendChild(button);
 }
 
 function collectConfig() {
-	// collect configuration from DOM
-	var mainTableBody = document.getElementById('mainTableBody');
-	var feeds = [];
-	for (var row = 0; row < mainTableBody.rows.length; row++) {
-		try {
-			var url_regex = mainTableBody.rows[row].querySelector('.url_regex').value.trim();
-			var check = mainTableBody.rows[row].querySelector('.activ').checked;
-			if(url_regex !== '' ) {
-				feeds.push({
-					'activ': check,
-					'url_regex': url_regex
-				});
-			}
-		}catch(e){
-			console.error(e);
-		}
-	}
-	return feeds;
+  // collect configuration from DOM
+  var mainTableBody = document.getElementById("mainTableBody");
+  var feeds = [];
+  for (var row = 0; row < mainTableBody.rows.length; row++) {
+    try {
+      var url_regex = mainTableBody.rows[row]
+        .querySelector(".url_regex")
+        .value.trim();
+      var check = mainTableBody.rows[row].querySelector(".activ").checked;
+      if (url_regex !== "") {
+        feeds.push({
+          activ: check,
+          url_regex: url_regex,
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return feeds;
 }
 
 function createButton(text, id, callback, submit) {
-	var span = document.createElement('span');
-	var button = document.createElement('button');
-	button.id = id;
-	button.textContent = text;
-	button.className = "browser-style";
-	if (submit) {
-		button.type = "submit";
-	} else {
-		button.type = "button";
-	}
-	button.name = id;
-	button.value = id;
-	button.addEventListener("click", callback);
-	span.appendChild(button);
-	return span;
+  var span = document.createElement("span");
+  var button = document.createElement("button");
+  button.id = id;
+  button.textContent = text;
+  button.className = "browser-style";
+  if (submit) {
+    button.type = "submit";
+  } else {
+    button.type = "button";
+  }
+  button.name = id;
+  button.value = id;
+  button.addEventListener("click", callback);
+  span.appendChild(button);
+  return span;
 }
 
 async function saveOptions() {
-	var feeds = collectConfig();
-	await browser.storage.local.set({ 'selectors': feeds });
+  var feeds = collectConfig();
+  await browser.storage.local.set({ selectors: feeds });
 }
 
 async function restoreOptions() {
+  onLoad();
 
-	onLoad();
-
-	createTableRow({
-		'activ': 1,
-		'url_regex': '',
-		'action':'save'
-	});
-	var res = await browser.storage.local.get('selectors');
-	if ( !Array.isArray(res.selectors) ) { return; }
-	res.selectors.forEach( (selector) => {
-		selector.action = 'delete'
-		createTableRow(selector);
-	});
+  createTableRow({
+    activ: 1,
+    url_regex: "",
+    action: "save",
+  });
+  var res = await browser.storage.local.get("selectors");
+  if (!Array.isArray(res.selectors)) {
+    return;
+  }
+  res.selectors.forEach((selector) => {
+    selector.action = "delete";
+    createTableRow(selector);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
 
-const impbtnWrp = document.getElementById('impbtn_wrapper');
-const impbtn = document.getElementById('impbtn');
-const expbtn = document.getElementById('expbtn');
+const impbtnWrp = document.getElementById("impbtn_wrapper");
+const impbtn = document.getElementById("impbtn");
+const expbtn = document.getElementById("expbtn");
 
-expbtn.addEventListener('click', async function () {
-    var dl = document.createElement('a');
-    var res = await browser.storage.local.get('selectors');
-    var content = JSON.stringify(res.selectors);
-    dl.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(content));
-    dl.setAttribute('download', 'data.json');
-    dl.setAttribute('visibility', 'hidden');
-    dl.setAttribute('display', 'none');
-    document.body.appendChild(dl);
-    dl.click();
-    document.body.removeChild(dl);
+expbtn.addEventListener("click", async function () {
+  var dl = document.createElement("a");
+  var res = await browser.storage.local.get("selectors");
+  var content = JSON.stringify(res.selectors);
+  dl.setAttribute(
+    "href",
+    "data:application/json;charset=utf-8," + encodeURIComponent(content)
+  );
+  dl.setAttribute("download", "data.json");
+  dl.setAttribute("visibility", "hidden");
+  dl.setAttribute("display", "none");
+  document.body.appendChild(dl);
+  dl.click();
+  document.body.removeChild(dl);
 });
 
 // delegate to real Import Button which is a file selector
-impbtnWrp.addEventListener('click', function() {
-	impbtn.click();
-})
+impbtnWrp.addEventListener("click", function () {
+  impbtn.click();
+});
 
-impbtn.addEventListener('input', function () {
-	var file  = this.files[0];
-	var reader = new FileReader();
-	reader.onload = async function() {
-        try {
-            var config = JSON.parse(reader.result);
-            await browser.storage.local.set({ 'selectors': config});
-            document.querySelector("form").submit();
-        } catch (e) {
-            console.error('error loading file: ' + e);
-        }
-    };
-    reader.readAsText(file);
+impbtn.addEventListener("input", function () {
+  var file = this.files[0];
+  var reader = new FileReader();
+  reader.onload = async function () {
+    try {
+      var config = JSON.parse(reader.result);
+      await browser.storage.local.set({ selectors: config });
+      document.querySelector("form").submit();
+    } catch (e) {
+      console.error("error loading file: " + e);
+    }
+  };
+  reader.readAsText(file);
 });
